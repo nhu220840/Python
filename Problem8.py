d22c71349eafee346c2d3f0855fc7c7e4e86814f
nhu220840 <nhu220840@gmail.com>
2023-12-16 17:04:37 +0700

    fix problem 8

--- Problem8.py
+++ Problem8.py
@@ -5,7 +5,6 @@ elif a < b:
     print("There are all the number from a to b: ", end = '')
     for i in range (a, b + 1):
         print(i, end = ' ')
-    print("\n")
 else:
     print("There are all the number from b to a: ", end = '')
     for i in range (a, b - 1, -1):
