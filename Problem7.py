2c6031d8e195a582ab697662c061f7717b60aa8e
nhu220840 <nhu220840@gmail.com>
2024-01-13 21:40:26 +0700

    update file

--- Problem7.py
+++ Problem7.py
@@ -1,2 +1,9 @@
 a, b, c = map(int, input("Please enter 3 positive integers a, b, c: ").split())
-print("The largest number out of those 3 numbers is:", max(a, b, c))
+max = a
+if b > max:
+    print(b, "is the largest")
+elif c > max:
+    print(c, "is the largest")
+else:
+    print(max, "is the largest")
+#print("The largest number out of those 3 numbers is:", max(a, b, c))
--- Problem8.py
+++ Problem8.py
@@ -1,4 +1,5 @@
 a, b = int(input("Please enter a positive integer a: ")), int(input("Please enter a positive integer b: "))
+
 if a == b:
     print("Two numbers are equal")
 elif a < b:
