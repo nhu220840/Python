61965765403630d311e84308f0267dbbb58dc18f
nhu220840 <nhu220840@gmail.com>
2024-01-17 19:39:13 +0700

    update file

--- Learn Python/helloworld.py
+++ Learn Python/helloworld.py
@@ -1,5 +1,9 @@
-x = int(input())
+x, y, z, t = map(int, input().split())
 
-print(x)
-print("Hello World !")
-print("28tech C++ programming !")
+print(x, y, z, t, sep = "  ")
+print()
+print(y, z, x, t, sep = "--")
+print()
+print(2 * x, 3 * y, 4 * z, 5 * t, sep = ",")
+print()
+print("KET THUC !!")
