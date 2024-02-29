022800863f5c3ba0107d0c1b652d33a9e9589046
nhu220840 <nhu220840@gmail.com>
2024-02-29 10:09:24 +0700

    add assignemt 30

---
+++ Learn Python/Loops/30_KiemTraNhieuTestCase.py
@@ -0,0 +1,16 @@
+cases = int(input())
+
+# while cases != 0:
+#     num = int(input())
+#     if num % 2 == 0:
+#         print("EVEN")
+#     else:
+#         print("ODD")
+#     cases -= 1
+
+for i in range(cases):
+    num = int(input())
+    if num % 2 == 0:
+        print("EVEN")
+    else:
+        print("ODD")
