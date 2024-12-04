41fde10959e4f315cc621aa11360dbab5e8452fc
nhu220840 <nhu220840@gmail.com>
2024-12-04 16:41:58 +0700

    update

---
+++ learn_tuples.py
@@ -0,0 +1,16 @@
+#Tuple
+
+fruits = ("apple", "orange", "banana", "coconut", "coconut")
+
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+print(fruits.index("apple"))
+
+# fruits.pop()
+# print("pineapple" in fruits)
+print(fruits.count("coconut"))
+
+print(fruits)
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+groceries = [["apple", "orange", "banana", "coconut"],
+#              ["celary", "carrots", "potatoes"],
+#              ["chicken", "fish", "turkey"]]
+
+# # print(groceries[0][0])
+# for collection in groceries:
+#     for food in collection:
+#         print(food, end = " ")
+#     print()
