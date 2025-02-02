0253c11e41e5136458a8283de9e6844cf785545e
nhu220840 <nhu220840@gmai.com>
2025-02-02 15:46:45 +0700

    update file demo for final exam

---
+++ Labwork1/final_demo.py
@@ -0,0 +1,36 @@
+import threading
+
+class Character:
+    def __init__(self, name, level, health):
+        self.name = name
+        self.level = level
+        self.health = health
+
+class PlayerCharacter(Character):
+    def __init__(self, name, level, health, inventory):
+        super().__init__(name, level, health)
+        self.inventory = inventory
+
+    def __str__(self):
+        return f"Name: {self.name}\nLevel: {self.level}\nHealth: {self.health}\nInventory: {self.inventory}"
+    
+    def save(self, filename):
+        try:
+            with open(filename, "w") as f:
+                f.write(f"Name: {self.name}\nLevel: {self.level}\nHealth: {self.health}\nInventory: {self.inventory}\n")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def save_in_background(self, filename):
+        t = threading.Thread(target=self.save, args=(filename,))
+        t.start()
+
+test = Character("Stone Giant", 1, 780)
+
+#Test case
+test2 = PlayerCharacter("Gandalf", 99, 1000, "Staff of Power")
+print(test2)
+test2.save("./Labwork1/player_info.txt")
+test2.save_in_background("./Labwork1/player.txt")
+
+
---
+++ Labwork1/retake.py
@@ -0,0 +1,45 @@
+import os
+
+class Reservation:
+    def __init__(self, guest, paid, room):
+        self.guest = guest
+        self.paid = paid
+        self.room = room
+
+    def greet(self):
+        return f"Hello, {self.guest}! - Room: {self.room}"
+    
+class LongReservation(Reservation):
+    def __init__(self, guest, paid, room, months):
+        super().__init__(guest, paid, room)
+        self.months = months
+
+    def greet(self):
+        return f"Hello, {self.guest}! - Room: {self.room} - {self.months} months"
+    
+    def save(self, filename):
+        try:
+            with open(filename, "w") as f:
+                f.write(f"Guest: {self.guest}\nPaid: {self.paid}\nRoom: {self.room}\nMonths: {self.months}\n")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def save_compress(self, filename):
+        self.save(filename)
+
+        os.system(f"zip 'output.zip' {filename}")
+
+    
+customer = Reservation("Emmanuel Macron", True, "R408")
+
+#Test case
+# print(customer.greet())
+
+# long_term_customer1 = LongReservation("Angela Merkel", False, "S502", 12)
+# print(long_term_customer1.greet())
+
+# long_term_customer1.save("./Labwork1/reservation_info.txt") 
+# long_term_customer1.save_compress("./Labwork1/reservation_info2.txt") 
+
+
+
